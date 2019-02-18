766ec27c4d621d79d279a1c806db0a3b03343a8c
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-18 15:25:38 -0500

    added basic searchbar

---
+++ instagram/src/components/SearchBar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class SearchBar extends Component {
+	render() {
+		return (
+			<div>
+				x
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
