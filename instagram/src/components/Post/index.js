9139cb59c9f62ba35cee15657245c1cc568b00f0
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-20 15:23:27 -0500

    added the ability to like the post only once with hooks

--- instagram/src/components/Post/index.js
+++ instagram/src/components/Post/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,35 +7,43 @@ import moment from 'moment';
 import { faHeart, faComment } from '@fortawesome/free-regular-svg-icons';
 
 const Post = (props) => {
-    const timestamp = moment(props.post.timestamp, "MMMM Do YYYY, h:mm:ss a").fromNow();
-    return (
-        <div className="post">
-            <div className="post-top">
-                <img src={props.post.thumbnailUrl} alt="thumbnail" />
-                <p>{props.post.username}</p>
-            </div>
-            <div className="post-mid">
-                <img src={props.post.imageUrl} alt='post' />
-            </div>
-            <div className="post-bottom">
-                <FontAwesomeIcon icon={faHeart} className="icon" onClick={(e) => props.likePost(e, props.post.timestamp)} />
-                <FontAwesomeIcon icon={faComment} className="icon" />
-                <p className='likes'>{props.post.likes} likes</p>
-            </div>
-            <CommentSection comments={props.post.comments} />
+	const timestamp = moment(props.post.timestamp, "MMMM Do YYYY, h:mm:ss a").fromNow();
+	const [liked, setLiked] = useState(false);
+	return (
+		<div className="post">
+			<div className="post-top">
+				<img src={props.post.thumbnailUrl} alt="thumbnail" />
+				<p>{props.post.username}</p>
+			</div>
+			<div className="post-mid">
+				<img src={props.post.imageUrl} alt='post' />
+			</div>
+			<div className="post-bottom">
+				<FontAwesomeIcon icon={faHeart} className="icon"
+					onClick={(e) => {
+						if(!liked){
+							setLiked(true);
+							props.likePost(e, props.post.timestamp);
+						}
+					}
+				} />
+				<FontAwesomeIcon icon={faComment} className="icon" />
+				<p className='likes'>{props.post.likes} likes</p>
+			</div>
+			<CommentSection comments={props.post.comments} />
 
-            <div className="time">
-                <p>{timestamp.toUpperCase()}</p>
-            </div>
+			<div className="time">
+				<p>{timestamp.toUpperCase()}</p>
+			</div>
 
-            <form className="new-comment" onSubmit={(e) => props.addNewComment(e, props.post.timestamp)}>
-                <input
-                    placeholder="Add a comment..."
-                    name="newComment"
-                />
-            </form>
-        </div>
-    );
+			<form className="new-comment" onSubmit={(e) => props.addNewComment(e, props.post.timestamp)}>
+				<input
+					placeholder="Add a comment..."
+					name="newComment"
+				/>
+			</form>
+		</div>
+	);
 }
 
 Post.propTypes = {
